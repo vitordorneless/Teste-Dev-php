462cfe9dd0d88ba1a3d8c65fcd6f3bbf621bb551
vitordorneless <vitordorneles@hotmail.com>
2025-05-08 11:51:36 -0300

    feat: criando migration clients

--- apiclientes/database/migrations/2025_05_08_143223_clients.php
+++ apiclientes/database/migrations/2025_05_08_143223_clients.php
@@ -11,13 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
+            $table->string('doc_number')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('district')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('zip_code')->nullable();
+            $table->string('password');            
             $table->timestamps();
         });
     }
